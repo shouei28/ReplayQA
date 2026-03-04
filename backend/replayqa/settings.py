f865a376745b4849042d4f8acc66e81ad7b41af6
shouei <shouei@cs.washington.edu>
2026-03-04 13:16:07 -0800

    Added more ports to the list

--- backend/replayqa/settings.py
+++ backend/replayqa/settings.py
@@ -153,6 +153,18 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
+    "http://localhost:3002",
+    "http://127.0.0.1:3002",
+    "http://localhost:3003",    
+    "http://127.0.0.1:3003",
+    "http://localhost:3004",
+    "http://127.0.0.1:3004",
+    "http://localhost:3005",
+    "http://127.0.0.1:3005",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
