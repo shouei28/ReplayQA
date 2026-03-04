970b252e269813b6a9c9aa0530e8073efd9a863c
shouei <shouei@cs.washington.edu>
2026-03-04 13:48:48 -0800

    Fixed the lint issue

--- backend/replayqa/settings.py
+++ backend/replayqa/settings.py
@@ -159,12 +159,12 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3001",
     "http://localhost:3002",
     "http://127.0.0.1:3002",
-    "http://localhost:3003",    
+    "http://localhost:3003",
     "http://127.0.0.1:3003",
     "http://localhost:3004",
     "http://127.0.0.1:3004",
     "http://localhost:3005",
-    "http://127.0.0.1:3005"
+    "http://127.0.0.1:3005",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
