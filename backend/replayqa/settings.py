c56c9ce1e6f39cd2b3625a6d5e355687443743ea
shouei <shouei@cs.washington.edu>
2026-03-10 16:11:48 -0700

    Fixed it so that it can be set using their own port locally

--- backend/replayqa/settings.py
+++ backend/replayqa/settings.py
@@ -150,22 +150,10 @@ REST_FRAMEWORK = {
 }
 
 # CORS settings
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "http://localhost:3001",
-    "http://127.0.0.1:3001",
-    "http://localhost:3002",
-    "http://127.0.0.1:3002",
-    "http://localhost:3003",
-    "http://127.0.0.1:3003",
-    "http://localhost:3004",
-    "http://127.0.0.1:3004",
-    "http://localhost:3005",
-    "http://127.0.0.1:3005",
-]
+CORS_ALLOWED_ORIGINS = os.getenv(
+"CORS_ALLOWED_ORIGINS",
+"http://localhost:3000,http://127.0.0.1:3000"
+).split(",")
 
 CORS_ALLOW_CREDENTIALS = True
 
